510e05b086e126b80e9645d5950b2ea6f1c4fcd5
maycon djeison <s.maycon@aluno.ifsp.edu.br>
2025-06-04 20:14:12 -0300

    Add files via upload

---
+++ exx1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+double 	x;
+double y;
+   cout << "digite valor 1 :";
+   cin >> x;
+   cout <<"digite valor 2 :";
+   cin>>y;
+   cout<<endl;
+   if (x>y)
+   {
+   	cout<<"o maior numero eh o " <<x;
+   }
+   else if (y>x)
+   {
+   	cout<<"o maior numero eh o "<<y;
+
+   }
+   cout<<" fim do programa"<<endl;
+	return 0;
+}
